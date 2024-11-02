a6a44d2417db842a458285c966587189bc2b55f6
00019323 <osetimbetov@students.wiut.uz>
2024-11-02 23:46:24 +0500

    Connections string added

--- WAD.Codebase.00019323/Program.cs
+++ WAD.Codebase.00019323/Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using WAD.Codebase._00019323.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionString")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
